0e91b092912caac0a538580daa8c87efeba6af75
Aleksandr Muzhev <muzhev1990@mail.ru>
2026-06-28 14:41:04 +0500

    fix: update babel and webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,43 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
-        clean: true,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
+module.exports = (env, argv) => {
+    const isProduction = argv.mode === 'production';
+
+    return {
+        entry: './src/index.js',
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'bundle.js',
+            clean: true,
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.css$/,
+                    use: ['style-loader', 'css-loader']
+                },
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader'
                     }
                 }
-            },
-            {
-                test: /\.html$/,
-                use: ['html-loader']
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-            filename: 'index.html'
-        })
-    ],
-    devServer: {
-        static: './dist',
-        port: 8080,
-        open: true
-    }
+            ]
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: './src/index.html',
+                filename: 'index.html'
+            })
+        ],
+        devServer: {
+            static: './dist',
+            port: 8080,
+            open: true
+        },
+        mode: isProduction ? 'production' : 'development',
+        devtool: isProduction ? false : 'inline-source-map'
+    };
 };
